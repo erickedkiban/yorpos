879f6ebc48c0ebd2d3392c2d30cb9e56eb450a9c
jaraldwin <ppoymae13@gmail.com>
2023-06-21 15:45:16 +0800

    update PaymentPage.vue

--- src/pages/PaymentPage.vue
+++ src/pages/PaymentPage.vue
@@ -1,39 +1,127 @@
 <template>
   <q-page class="q-ma-lg" style="min-height: 0 !important">
     <div>
-      <div class="text-h4 text-weight-light text-uppercase q-mb-md">
-        order #: 12564878
-      </div>
-      <q-separator style="color: #d8dbd9" />
       <div class="row">
-        <div class="q-mt-md col-8">
-          <q-table
-            grid
-            grid-header
-            flat
-            bordered
-            :rows="rows"
-            :columns="columns"
-            row-key="name"
-            hide-header
-          >
-          </q-table>
-          <q-btn
-            push
-            class="text-uppercase text-weight-light full-width text-h6"
-            color="red"
-            label="Cancel Order"
-          />
+        <div class="col-8">
+          <div v-for="orderId in Object.keys(segregatedOrders)" :key="orderId">
+            <div class="text-h5 text-weight-light text-uppercase q-mb-sm">
+              order #: {{ orderId }}
+            </div>
+            <div class="flex justify-end">
+              <q-checkbox style="position: relative; top: -10px;" v-model="selectedTables" :val="orderId" dense />
+            </div>
+            <q-separator style="color: #d8dbd9" />
+            <q-table
+              grid
+              grid-header
+              flat
+              bordered
+              :rows="segregatedOrders[orderId]"
+              :columns="columns"
+              row-key="name"
+              hide-header
+            >
+              <template v-slot:bottom>
+                <div class="q-pt-lg q-pb-lg" style="width: 100%">
+                  <q-btn
+                    push
+                    class="text-uppercase text-black text-weight-light full-width text-h6"
+                    color="red"
+                    label="Cancel Order"
+                    @click="cancelOrder(orderId)"
+                  />
+                </div>
+              </template>
+            </q-table>
+          </div>
         </div>
-        <div class="q-mt-md col-4"></div>
+        <div class="col-4">
+          <div class="q-pl-xl">
+            <div class="text-h5 text-weight-light text-uppercase">
+              Payable Amount
+            </div>
+            <div
+              class="text-h5 text-weight-light text-uppercase q-mb-md"
+              style="color: #d89f65"
+            >
+              P{{ totalPrice }}
+            </div>
+            <q-separator style="color: #d8dbd9" />
+            <div class="flex justify-between q-mt-lg">
+              <!-- Remaining code omitted for brevity -->
+            </div>
+            <div>
+              <q-card class="q-mt-lg" style="background: #eaf0f0">
+                <q-card-section>
+                  <div class="flex justify-between items-center">
+                    <div>
+                      <div class="text-subtitle2 text-uppercase">
+                        Add Cash Received
+                      </div>
+                    </div>
+                    <div>
+                      <q-input
+                        :input-style="{ fontSize: '20px' }"
+                        v-model="cashReceived"
+                        :dense="dense"
+                      />
+                    </div>
+                  </div>
+                </q-card-section>
+              </q-card>
+            </div>
+            <div class="q-pt-md q-pb-md">
+              <div
+                class="text-subtitle1 text-uppercase text-weight-medium flex justify-between"
+              >
+                <div>Subtotal</div>
+                <div>P{{ totalPrice }}</div>
+              </div>
+              <div
+                class="text-subtitle1 text-uppercase text-weight-medium flex justify-between"
+              >
+                <div>Change</div>
+                <div v-if="cashReceived > totalPrice">
+                  P{{ cashReceived - totalPrice }}
+                </div>
+                <div v-else>P0</div>
+              </div>
+              <div
+                class="text-subtitle1 text-uppercase text-weight-medium flex justify-between"
+              >
+                <div>
+                  Service Charge
+                  <span class="text-weight-light q-ml-sm">0%</span>
+                </div>
+                <div>0</div>
+              </div>
+            </div>
+            <q-separator class="q-mb-xl" style="color: #d8dbd9" />
+            <div
+              class="flex justify-between text-uppercase text-weight-light text-h5"
+            >
+              <div>Total</div>
+              <div>P{{ totalPrice }}</div>
+            </div>
+            <div class="q-mt-xl">
+              <q-btn
+                style="background: #8bb5be; width: 100%"
+                class="text-uppercase text-h5"
+              >
+                Pay Now
+              </q-btn>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </q-page>
 </template>
+
 <script>
-import { ref, getCurrentInstance, onMounted } from "vue";
-import { initializeApp, getApp } from "firebase/app";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { ref, getCurrentInstance, onMounted, computed, watch } from "vue";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import {
   getFirestore,
   collection,
@@ -46,6 +134,7 @@ import {
   updateDoc,
   getDocs,
 } from "firebase/firestore";
+
 const columns = [
   {
     name: "item",
@@ -59,100 +148,127 @@ const columns = [
     name: "price",
     align: "center",
     label: "PRICE",
-    field: "price",
+    field: (row) => row.price,
     sortable: true,
   },
-  { name: "qty", label: "QTY", field: "qty", sortable: true },
-  { name: "subtotal", label: "SUBTOTAL", field: "subtotal" },
-];
-
-const rows = [
   {
-    name: "Frozen Yogurt",
-    price: 159,
-    qty: 6.0,
-    subtotal: 24,
-  },
-  {
-    name: "Frozen Yogurt",
-    price: 159,
-    qty: 6.0,
-    subtotal: 24,
-  },
-  {
-    name: "Frozen Yogurt",
-    price: 159,
-    qty: 6.0,
-    subtotal: 24,
-  },
-  {
-    name: "Frozen Yogurt",
-    price: 159,
-    qty: 6.0,
-    subtotal: 24,
-  },
-  {
-    name: "Frozen Yogurt",
-    price: 159,
-    qty: 6.0,
-    subtotal: 24,
+    name: "qty",
+    label: "QTY",
+    field: (row) => row.order_quantity,
+    sortable: true,
   },
 ];
 
 export default {
   setup() {
+    const selectedTables = ref([]);
+    const cashReceived = ref(null);
     const uuid = ref("");
     const orders = ref([]);
     const app = getCurrentInstance().appContext.config.globalProperties;
     const app1 = initializeApp(app.$firebaseConfig);
     const db = getFirestore(app1);
 
-    // async function getData() {
-    //   const querySnapshot = await getDocs(collection(db, "orders"));
-    //   querySnapshot.forEach((doc) => {
-    //     // doc.data() is never undefined for query doc snapshots
-    //     console.log(doc.id, " => ", doc.data());
-    //   });
-    // }
+    const selectedOrders = computed(() => {
+      const selected = [];
+      for (const orderId of selectedTables.value) {
+        if (segregatedOrders.value.hasOwnProperty(orderId)) {
+          selected.push(...segregatedOrders.value[orderId]);
+        }
+      }
+      return selected;
+    });
+
+    const totalPrice = computed(() => {
+      return selectedOrders.value.reduce(
+        (total, item) => total + item.total,
+        0
+      );
+    });
+
+    const segregatedOrders = computed(() => {
+      const segregated = {};
+
+      orders.value.forEach((order) => {
+        const orderId = order.orderId;
+
+        if (!segregated[orderId]) {
+          segregated[orderId] = [];
+        }
+        const items = order.orders.map((item) => {
+          const total = item.price * item.order_quantity;
+          return { ...item, total };
+        });
+        segregated[orderId].push(...items);
+      });
+
+      return segregated;
+    });
 
     async function getData() {
-      // Get the current user ID
       const currentUser = getAuth().currentUser;
       if (currentUser) {
         const q = query(collection(db, "orders"));
-        console.log("currentUser.uid", currentUser.uid);
-
         const querySnapshot = await getDocs(q);
+        const matchingOrders = [];
+
         querySnapshot.docs.forEach((doc) => {
-          // Access the document data using doc.data()
           const orderData = doc.data();
 
-          if (Array.isArray(orderData.orders) && orderData.orders.length > 0) {
-            const matchingOrder = orderData.orders.find(
-              (order) => order.userid === currentUser.uid
-            );
-
-            if (matchingOrder) {
-              console.log("Matching Order:", matchingOrder);
-            }
-            else {
-              console.log("No matching Order:", currentUser.uid)
-            }
+          if (
+            Array.isArray(orderData.orders) &&
+            orderData.orders.length > 0 &&
+            orderData.userId === currentUser.uid &&
+            orderData.payment_status === "unpaid"
+          ) {
+            matchingOrders.push(orderData);
           }
         });
+
+        orders.value = matchingOrders;
       }
     }
 
-    onMounted(() => {
-      getData();
+    async function cancelOrder(orderId) {
+      const orderQuery = query(
+        collection(db, "orders"),
+        where("orderId", "==", orderId)
+      );
+      const querySnapshot = await getDocs(orderQuery);
+
+      querySnapshot.forEach(async (doc) => {
+        // Delete the order document
+        await deleteDoc(doc.ref);
+      });
+
+      // Remove the order from the orders array
+      orders.value = orders.value.filter((order) => order.orderId !== orderId);
+    }
+
+    watch(selectedTables, (newValue) => {
+      if (newValue.length === 0) {
+        cashReceived.value = null;
+      }
     });
 
+    onMounted(getData);
+
     return {
-      getData,
+      selectedTables,
+      cashReceived,
       uuid,
+      orders,
       columns,
-      rows,
+      selectedOrders,
+      totalPrice,
+      segregatedOrders,
+      cancelOrder,
+      dense: ref(false),
     };
   },
 };
 </script>
+
+<style>
+/* Your custom styles here */
+</style>
